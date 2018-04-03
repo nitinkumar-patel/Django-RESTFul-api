48ffddef76983df99ac0e2e28e813dec416754e4
Nitin <er.nitinpatel914@gmail.com>
2018-04-03 07:43:27 -0500

    django restful api project added

---
+++ movierater/api/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+from movierater.api.models import Movie, Rating
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ('id', 'title', 'description', 'avg_rating', 'no_of_ratings')
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ('stars', 'user', 'movie')
---
+++ movierater/api/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render
+
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, status
+from movierater.api.serializers import UserSerializer, MovieSerializer, RatingSerializer
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from movierater.api.models import Movie, Rating
+from rest_framework.decorators import list_route
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    # authentication_classes = (TokenAuthentication, SessionAuthentication)
+    # permission_classes = (IsAuthenticated,)
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    authentication_classes = (TokenAuthentication, SessionAuthentication)
+    permission_classes = (IsAuthenticated,)
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+    authentication_classes = (TokenAuthentication, SessionAuthentication)
+    permission_classes = (IsAuthenticated,)
+
+    @list_route(methods=['post'])
+    def rate_movie(self, request):
+        if 'movie' in request.data and 'user' in request.data and 'stars' in request.data:
+            movie = Movie.objects.get(id=request.data['movie'])
+            user = User.objects.get(id=request.data['user'])
+            stars = request.data['stars']
+
+            try:
+                my_rating = Rating.objects.get(movie=movie.id, user=user.id)
+                my_rating.stars = stars
+                my_rating.save()
+                serializer = MovieSerializer(movie, many=False)
+                response = {"message": "Rating updated", "result": serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                Rating.objects.create(movie=movie, user=user, stars=stars)
+                serializer = MovieSerializer(movie, many=False)
+                response = {"message": "Rating created", "result": serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+                pass
+        else:
+            response = {"message": "you need to pass all the params"}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CustomObtainAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super(CustomObtainAuthToken, self).post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        user = User.objects.get(id=token.user_id)
+        serializer = UserSerializer(user, many=False)
+        return Response({'token': token.key, 'user': serializer.data})
